66004b57b1513acef7ae861a0aa39ce7ed1cec96
Rich <rchien@outlook.com>
2017-06-14 17:01:16 -0700

    removed main.R

--- ds_MEPS/global.R
+++ ds_MEPS/global.R
@@ -12,12 +12,12 @@ packages <- c(
 )
 
 
-missing.packages <- setdiff(packages, rownames(installed.packages()))
+#missing.packages <- setdiff(packages, rownames(installed.packages()))
 
-if (length(missing.packages) > 0) {
-cat(c('The following packages will be installed:\n\t', missing.packages, '\n'))
-install.packages(missing.packages, repos="http://cran.r-project.org", dependencies = TRUE)
-}
+#if (length(missing.packages) > 0) {
+#cat(c('The following packages will be installed:\n\t', missing.packages, '\n'))
+#install.packages(missing.packages, repos="http://cran.r-project.org", dependencies = TRUE)
+#}
 
 sapply(packages, require, character.only = TRUE)
 
